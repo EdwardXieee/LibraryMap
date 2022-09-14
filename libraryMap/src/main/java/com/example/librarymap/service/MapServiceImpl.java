ed5870dd6bfc4703455d400fffa090920b30cab3
YueningXie <sayyx3@nottingham.edu.cn>
2022-09-14 19:26:16 +0800

    feat:

--- libraryMap/src/main/java/com/example/librarymap/service/MapServiceImpl.java
+++ libraryMap/src/main/java/com/example/librarymap/service/MapServiceImpl.java
@@ -3,9 +3,11 @@ package com.example.librarymap.service;
 import com.example.librarymap.mapper.LibraryMapMapper;
 import com.example.librarymap.pojo.LibraryMap;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MapServiceImpl implements MapService{
 
     @Autowired
